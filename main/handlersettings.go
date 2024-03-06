e8a80d1dd53c3b3f3b75fd5da714691f257c2470
Joseph Calev <jcalev@microsoft.com>
2024-03-06 14:35:42 -0800

    Removes schema validation (#30)

--- main/handlersettings.go
+++ main/handlersettings.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-
 	"github.com/go-kit/kit/log"
 	"github.com/pkg/errors"
 )
@@ -165,12 +163,6 @@ func parseAndValidateSettings(ctx *log.Context, configFilePath string) (h handle
 	}
 	ctx.Log("event", "read configuration")
 
-	ctx.Log("event", "validating json schema")
-	if err := validateSettingsSchema(pubJSON, protJSON); err != nil {
-		return h, errors.Wrap(err, "json validation error")
-	}
-	ctx.Log("event", "json schema valid")
-
 	ctx.Log("event", "parsing configuration json")
 	if err := UnmarshalHandlerSettings(pubJSON, protJSON, &h.publicSettings, &h.protectedSettings); err != nil {
 		return h, errors.Wrap(err, "json parsing error")
@@ -192,34 +184,4 @@ func readSettings(configFilePath string) (pubSettingsJSON, protSettingsJSON map[
 	pubSettingsJSON, protSettingsJSON, err = ReadSettings(configFilePath)
 	err = errors.Wrapf(err, "error reading extension configuration")
 	return
-}
-
-// validateSettings takes publicSettings and protectedSettings as JSON objects
-// and runs JSON schema validation on them.
-func validateSettingsSchema(pubSettingsJSON, protSettingsJSON map[string]interface{}) error {
-	pubJSON, err := toJSON(pubSettingsJSON)
-	if err != nil {
-		return errors.Wrap(err, "failed to unmarshal public settings into json")
-	}
-	protJSON, err := toJSON(protSettingsJSON)
-	if err != nil {
-		return errors.Wrap(err, "failed to unmarshal protected settings into json")
-	}
-
-	if err := validatePublicSettings(pubJSON); err != nil {
-		return err
-	}
-	if err := validateProtectedSettings(protJSON); err != nil {
-		return err
-	}
-	return nil
-}
-
-// toJSON converts given in-memory JSON object representation into a JSON object string.
-func toJSON(o map[string]interface{}) (string, error) {
-	if o == nil { // instead of JSON 'null' assume empty object '{}'
-		return "{}", nil
-	}
-	b, err := json.Marshal(o)
-	return string(b), errors.Wrap(err, "failed to marshal into json")
 }
