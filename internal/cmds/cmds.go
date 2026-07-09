95ad9351369beb2fbf70221c6e90c2d6cca3fee3
alsanmsft <anasanc@microsoft.com>
2026-07-09 21:38:40 +0000

    nit change

--- internal/cmds/cmds.go
+++ internal/cmds/cmds.go
@@ -225,9 +225,10 @@ func enable(ctx *log.Context, h types.HandlerEnvironment, report *types.RunComma
 		if err != nil {
 			// TODO: In the future, fail the command if the policy file was invalid and return the exitCode.
 			// For now, log the error and continue with the command execution.
-			ctx.Log("error", "failed to initialize extension policy settings. No policy applied.", "error", err, "errorCode", exitCode)
+			ctx.Log("error", "failed to initialize extension policy settings. Executing command with no policy applied for now.", "error", err, "errorCode", exitCode)
+		} else {
+			ctx.Log("message", "successfully initialized extension policy settings")
 		}
-		ctx.Log("message", "successfully initialized extension policy settings")
 	} else if os.IsNotExist(err) {
 		ctx.Log("message", "extension policy settings file does not exist. No policy applied.", "error", err)
 		extensionPolicyManagerPtr = nil
